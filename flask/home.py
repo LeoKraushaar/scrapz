dcda76f81100a445f4c9ef5825115cc4051cefc7
Leo Kraushaar <leo.kraushaar@gmail.com>
2025-02-15 13:42:01 -0700

    backup

--- flask/home.py
+++ flask/home.py
@@ -3,5 +3,10 @@ from flask import Flask, Blueprint, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<p>Welcome to SmartCart!</p>"
+
+@app.route('/pantry')
+def pantry():
+    return "<p>This is your pantry.</p"
+
