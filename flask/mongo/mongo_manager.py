82fe4bc74b9ce2134af79091c50480fc74ea0c9a
Leo Kraushaar <leo.kraushaar@gmail.com>
2025-02-15 14:11:56 -0700

    templates

--- flask/mongo/mongo_manager.py
+++ flask/mongo/mongo_manager.py
@@ -1,17 +1,25 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from pprint import pprint
 import os
-from constants import *
+# from ..constants import *
 
 class MongoManager:
 
     def __init__(self):
         self.client = MongoClient(os.getenv('MONGO_URI'))
         self.db = self.client[DB]
-        
-    def queryCollection(self, collection):
-        return list(self.db[collection])
+
+    def getCollection(self, coll_name:str):
+        return self.db[coll_name]
+
+    def queryCollection(self, coll_name:str):
+        coll = self.getCollection(coll_name)
+        return list(coll.find())
+
 
 if __name__ == '__main__':
+    print(os.getcwd())
     load_dotenv()
     mongo = MongoManager()
+    pprint(mongo.queryCollection(ITEMS))
