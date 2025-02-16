dd72c1cfb7ca5174c192b552322a3c3410880e77
Leo Kraushaar <leo.kraushaar@gmail.com>
2025-02-15 17:25:48 -0700

    mongomanger

--- flask/recipe_finder.py
+++ flask/recipe_finder.py
@@ -1,9 +1,19 @@
-
+from mongo.mongo_manager import MongoManager
 
 class RecipeFinder:
 
-	def __init__(self):
-		pass
+	def __init__(self, mongo:MongoManager):
+		self.mongo = mongo
 
 	def findRecipe(self):
 		pass
+
+	def suggestRecipe(self):
+		pass
+
+	def getOwnedItems(self):
+		items = 
+
+if __name__ == '__main__':
+	finder = RecipeFinder()
+	finder.getOwnedItems()
