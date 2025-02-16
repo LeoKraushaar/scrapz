bb31402ed730067998e4e69a61211f078b3cdf75
Heiby <whl@ualberta.ca>
2025-02-16 14:24:07 -0700

    added pantry.py

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint, render_template
 from mongodb.mongo_manager import MongoManager
+from pantry import pantry_bp
 from constants import *
 
 app = Flask(__name__)
 mongo = MongoManager(DB)
+
 
 @app.route("/")
 def home():
@@ -11,14 +13,16 @@ def home():
         'index.html'
     )
 
-@app.route('/pantry')
-def pantry():
-    items = mongo.queryCollection(ITEMS)
-    return render_template(
-            'pantry.html',
-            n=len(items),
-            items=items
-        )
+# @app.route('/pantry')
+# def pantry():
+#     items = mongo.queryCollection(ITEMS)
+#     return render_template(
+#             'pantry.html',
+#             n=len(items),
+#             items=items
+#         )
+
+app.register_blueprint(pantry_bp)
 
 @app.route('/recipes/results')
 def getRecipe():
